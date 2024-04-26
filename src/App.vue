0dca5f39c1c597b48537363905a8972baf10803d
Eren <f245986@gmail.com>
2024-04-26 20:22:43 +0800

    chore: initial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    <span class="text">nexify-test</span>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  border: 3px solid red;
+  .text {
+    color: red;
+  }
+}
+</style>
